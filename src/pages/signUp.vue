21726d4d013da23644afbaea93a1c02a80a43fb8
chenxiaosai <chenxiaosai@meituan.com>
2023-08-10 15:36:18 +0800

    sign

--- src/pages/signUp.vue
+++ src/pages/signUp.vue
@@ -1,26 +1,29 @@
 <template>
-  <div class="register-content">
-    <div class="title">
-      <h1>注册</h1>
-    </div>
-    <div class="forms">
-      <el-form ref="regFormRef" :model="regForm" status-icon :rules="rules" label-width="120px" class="demo-ruleForm" label-position="top">
-        <el-form-item label="用户名" prop="userName">
-          <el-input v-model="ruleForm.userName" type="password" autocomplete="off" />
-        </el-form-item>
-        <el-form-item label="手机号" prop="userPhone">
-          <el-input v-model="ruleForm.userPhone" type="userPhone" autocomplete="off" />
-        </el-form-item>
-        <el-form-item label="密码" prop="pass">
-          <el-input v-model="ruleForm.pass" type="password" autocomplete="off" />
-        </el-form-item>
-        <el-form-item label="再次确认密码" prop="checkPass">
-          <el-input v-model="ruleForm.checkPass" type="password" autocomplete="off" />
-        </el-form-item>
-        <el-form-item>
-          <el-button type="primary" @click="submitForm(regForm)">提交</el-button>
-        </el-form-item>
-      </el-form>
+  <div class="register">
+    <div class="register-content">
+      <div class="title">
+        <span>注册</span>
+      </div>
+      <div class="forms">
+        <el-form ref="regFormRef" :model="ruleForm" status-icon :rules="rules" label-width="120px" class="demo-ruleForm"
+          label-position="top">
+          <el-form-item label="用户名" prop="userName">
+            <el-input v-model="ruleForm.userName" type="text" autocomplete="off" />
+          </el-form-item>
+          <el-form-item label="手机号" prop="userPhone">
+            <el-input v-model="ruleForm.userPhone" type="userPhone" autocomplete="off" />
+          </el-form-item>
+          <el-form-item label="密码" prop="pass">
+            <el-input v-model="ruleForm.pass" type="password" autocomplete="off" />
+          </el-form-item>
+          <el-form-item label="再次确认密码" prop="checkPass">
+            <el-input v-model="ruleForm.checkPass" type="password" autocomplete="off" />
+          </el-form-item>
+          <el-form-item >
+            <el-button type="primary" @click="submitForm(regFormRef)" class="button">提交</el-button>
+          </el-form-item>
+        </el-form>
+      </div>
     </div>
   </div>
 </template>
@@ -31,14 +34,25 @@ import type { FormInstance, FormRules } from 'element-plus'
 
 const regFormRef = ref<FormInstance>()
 
+const validateName = (rule: any, value: any, callback: any) => {
+  if (value === '') {
+    callback(new Error('请输入用户名'))
+  } else {
+    callback()
+  }
+}
+
 const validatePhone = (rule: any, value: any, callback: any) => {
   var phoneRule = new RegExp(/^1[3456789]\d{9}$/)
-  if (value === ''){
+  if (value === '') {
     callback(new Error('请输入手机号'))
-  }else{
-    if (ruleForm.userPhone !== ''){
-      if (!phoneRule.test(value)) return
-      
+  } else {
+    if (ruleForm.userPhone !== '') {
+      if (!phoneRule.test(value)) {
+        callback(new Error('请输入正确的手机号'))
+      } else {
+        callback()
+      }
     }
   }
 }
@@ -72,8 +86,10 @@ const ruleForm = reactive({
 })
 
 const rules = reactive<FormRules<typeof ruleForm>>({
+  userName: [{ validator: validateName, trigger: 'blur' }],
   pass: [{ validator: validatePass, trigger: 'blur' }],
   checkPass: [{ validator: validatePass2, trigger: 'blur' }],
+  userPhone: [{ validator: validatePhone, trigger: 'blur' }],
 })
 
 const submitForm = (formEl: FormInstance | undefined) => {
@@ -89,4 +105,30 @@ const submitForm = (formEl: FormInstance | undefined) => {
 }
 </script>
 
-<style scoped></style>
+<style scoped lang="less">
+.register {
+  width: 40vw;
+  border: 1px solid silver;
+  display: flex;
+  justify-content: center;
+  border-radius: 10px;
+
+  .register-content {
+    width: 30vw;
+    margin:30px;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    .title{
+      font-size: 25px;
+      margin: 0 auto;
+      margin-bottom: 20px;
+    }
+    .button{
+      width: 100%;
+      margin: 0 auto;
+      margin-top: 30px;
+    }
+  }
+}
+</style>
